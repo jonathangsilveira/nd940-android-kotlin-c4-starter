a9206475ba98db8715b0aa66ce688e8e32c20e78
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2021-03-07 22:51:52 -0300

    Create a FakeDataSource to replace the Data Layer and test the app in isolation.

--- starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -3,25 +3,32 @@ package com.udacity.project4.locationreminders.data
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
-//Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
+class FakeDataSource(private val reminders: MutableList<ReminderDTO>) : ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
+    var shouldFail: Boolean = false
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        return if (shouldFail)
+            Result.Error("Test Exception")
+        else
+            Result.Success(reminders)
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        reminders.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        val reminder = reminders.find { it.id == id }
+        return when {
+            shouldFail -> Result.Error("Test Exception")
+            reminder == null -> Result.Error("Not found")
+            else -> Result.Success(reminder)
+        }
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        reminders.clear()
     }
 
 
